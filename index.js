cf93e2f7e3810f288aac853e7327be10c8f65945
RS <72138650+rishika-sharma@users.noreply.github.com>
2021-03-30 16:54:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,2 @@
 alert("Welcome To The Solar System");
 alert("Click On Get Started To View The Website");
-let Name = prompt("What Is Your Name ?");
-let age = parseInt(prompt("What Is Your Age ?"));
-let emailId = prompt("What Is Your email Id ?");
-
-function info() {
-    console.log(`Your Name Is ${Name}`);
-    console.log(`Your Age Is ${age}`);
-    console.log(`Your Email Id Is ${emailId}`);
-}
-info();
